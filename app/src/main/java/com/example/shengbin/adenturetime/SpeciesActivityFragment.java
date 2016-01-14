b8f745aa08c600f16225d278253856eed709ce05
Sandra <47419119l@iespoblenou.org>
2016-01-14 19:46:58 +0100

    Activitat SpeciesActivityFragment acabada.

--- app/src/main/java/com/example/shengbin/adenturetime/SpeciesActivityFragment.java
+++ app/src/main/java/com/example/shengbin/adenturetime/SpeciesActivityFragment.java
@@ -3,11 +3,13 @@ package com.example.shengbin.adenturetime;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -101,6 +103,16 @@ public class SpeciesActivityFragment extends Fragment {
 
 
 
+            }
+        });
+        
+        listCharacter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(getContext(), DetailsChacarter.class);
+                i.putExtra("item", adapter.getItem(position));
+                startActivity(i);
             }
         });
 
